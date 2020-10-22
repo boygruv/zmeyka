8bfdbdc6b0b8c7ef8553eadb880da624c17b52f8
Ermolaev Aleksey <aleksey.yermolayev@nexign.com>
2020-10-22 13:32:39 +0300

    Step 3

--- zmeyka.cpp
+++ zmeyka.cpp
@@ -84,6 +84,27 @@ void Draw()
 // Обработка нажатий клавиатуры
 void Input()
 {
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'b':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
 }
 
 // Основная логика
